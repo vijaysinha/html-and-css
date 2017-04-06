ec5426a075d2381a6073d15ae82f40634b22c875
vijaysinha60@gmail.com <vijaysinha60@gmail.com>
2017-04-06 23:18:56 +0530

    Updated map webapp 1.0 version.

--- js7.js
+++ js7.js
@@ -1,6 +1,5 @@
-var map;
 window.onload= getLocation;
-
+var map;
 function getLocation(){
 
 	if(navigator.geolocation)
@@ -19,20 +18,41 @@ function displayLocation(position){
 	var longitude = position.coords.longitude;
 	var div = document.getElementById("Location");
 
-	div.innerHTML = "Longitude:"+longitude+"<br>"+"Latitude:"+latitude+"<hr>";
+	div.innerHTML = "Longitude:"+longitude+"<br>"+"Latitude:"+latitude;
 	showMap(position.coords);
 }
 
-function showMap(position){
-	var googleLatAndLan = new google.maps.LatLng(position.latitude,position.longitude);
+function showMap(coords){
+
+	var googleLatAndLan = new google.maps.LatLng(coords.latitude,coords.longitude);
 	var mapOption = {
-		zoom:10;
-		center:googleLatAndLong;
-		mapTypeId:google.maps.mapTypeId.ROADMAP;
+		zoom: 10,
+		center: googleLatAndLong,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 
 	};
 	var divMap= document.getElementById("mapPage");
 	map = new google.maps.Map(divMap,mapOption);
-	//divMap.innerHTML=map;
+	
+	var title = "Your Location";
+	var content="You are"+ coords.latitude+ coords.longitude+"here !"; 
+	infoWindow.open(map);
 }
+function addMarker(map,latlong,title,content){
+	markerOption = {
+		position: latlong,
+		title: title,
+		map: map,
+		clickable: true
+	};
+	var marker = new google.maps.Marker(mapOption);
 
+	var infoWindowOption = {
+		content: content,
+		position: latlong
+	};
+	var infoWindow =  new google.maps.InfoWindow(infoWindowOption);
+	google.maps.event.addListener(marker,"click", function(){
+	infoWindow.open(map);
+});
+}
